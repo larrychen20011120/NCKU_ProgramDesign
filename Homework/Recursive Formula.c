983d55e23d6e1e634aad1cf9144f319cdfce46e3
LarryChen <38965858+larrychen20011120@users.noreply.github.com>
2020-11-19 14:31:31 +0800

    Add files via upload

---
+++ Homework/Recursive Formula.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int f(int a, int b, int n){
+
+}
+
+int main(){
+    int x1, x2, n;
+    while (1) {
+        scanf("%d%d%d", &x1, &x2, &n);
+        if (x1 == 0 && x2 == 0 && n == 0)
+            break;
+        for (int i = 3; i <= n; i++){
+            int temp = x2;
+            x2 += x1;
+            x1 = temp;
+        }
+        printf("%d\n", x2);
+    }
+    return 0;
+
+}
