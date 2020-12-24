858fe0a53bfddb6d76c0b01e64d36885a1770ead
LarryChen <38965858+larrychen20011120@users.noreply.github.com>
2020-12-24 13:57:55 +0800

    Add files via upload

---
+++ Homework/structure_pointer.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct Node{
+    int num;
+};
+void mul(struct Node* , struct Node, long long int *);
+
+int main(void){
+    struct Node node_a, node_b;
+    long long int ans = 1;
+
+    scanf("%d", &node_a.num);
+    scanf("%d", &node_b.num);
+    mul(&node_a, node_b, &ans);
+    printf("%lld", ans);
+
+    return 0;
+}
+// Your code goes here
+void mul(struct Node* node_a ,struct Node node_b, long long int* ans){
+    *ans = (*ans) * (*node_a).num;
+    *ans = (*ans) * node_b.num;
+}
